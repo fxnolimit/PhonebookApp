ccc8686458743b178712054c2a4738e813ac52d5
Francois (Franck) Mukaba <fxkikomina@gmail.com>
2018-10-08 16:51:57 -0700

    Update Phonebook.h

--- Phonebook.h
+++ Phonebook.h
@@ -1,7 +1,10 @@
 /*
  * Author name: Francois Mukaba
  * Created on: Oct 6, 2018
- */
+ * Course: CS 300
+ *
+ * 									Assignment 1/Phonebook.h
+ * */
 
 #ifndef PHONEBOOK_H_
 #define PHONEBOOK_H_
@@ -9,13 +12,14 @@
 
 class Phonebook {
 public:
-	unsigned const int incrementor = 160000; // to consistent memory allocation everytime the array is rebuilt
+	unsigned const int incrementor = 100; // to keep consistent memory allocation everytime the array is rebuilt
 	unsigned int size = 0; // number of elements in the array
 	unsigned int rebuild = 1; // number of time array has been built
-	Contact *storage; // array of pointers to store the contacts
+	Contact *storage; // array to store the contacts
 public:
 	Phonebook();
 	Phonebook(string);
+	long getLength();
 	void checkCapacity();
 	bool contains(string);
 	void add(string, long);
@@ -25,7 +29,6 @@ public:
 	void list();
 	long forceInputType_long();
 	void application();
-
 };
 
 #endif
